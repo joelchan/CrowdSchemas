17edb5b7d3880d2f58cf7755decb1cec9854f4f5
Joel Chan <chozen86@gmail.com>
2014-07-17 20:59:03 -0400

    added weighting schema for words, modified expand_words, next need to modify find_paths

--- graph_sim.py
+++ graph_sim.py
@@ -3,6 +3,22 @@ import numpy as np
 import itertools as it
 import os, csv
 
+class enhancedWord:
+    def __init__(self, synset, level, weight):
+        self.synset = synset
+        self.level = level
+        self.weight = weight
+    def __str__(self):
+        return self.synset.name + " with level " + str(self.level) + " and weight " + str(self.weight)
+
+class rawWord:
+    def __init__(self, label, posTag, weight):
+        self.label = label
+        self.pos = get_wordnet_pos(posTag)
+        self.weight = weight
+    def __str__(self):
+        return self.label
+
 def read_data(filename):
 	"""
 	Read in data from a file and return a list with each element being one line from the file.
@@ -63,9 +79,10 @@ def expand_words(words):
 	
 	synonyms = set()
 	for item in words:
-		synsets = wn.synsets(item[0],get_wordnet_pos(item[1]))
+		synsets = wn.synsets(item.label,item.pos)
+		itemWeight = item.weight # divide weight among the synonyms? maybe item.weight/len(synsets)
 		for synset in synsets:
-			synonyms.add((synset,1))
+			synonyms.add(enhancedWord(synset,1,itemWeight))
 
 	# get derivationally related forms for "properties"
 	derivations = set()
@@ -73,7 +90,7 @@ def expand_words(words):
 		if synonym[0].pos == 's':
 			for lemma in synonym[0].lemmas:
 				for form in lemma.derivationally_related_forms():
-					derivations.add((form.synset,1))
+					derivations.add(enhancedWord(form.synset,1,synonym.weight))
 	synonyms.update(derivations)
 
 	# add to master
@@ -88,12 +105,13 @@ def expand_words(words):
 		# get all hypernyms, put in nextStack
 		for s in currentStack:
 			for hypernym in s[0].hypernyms():
-				nextStack.add((hypernym,level))
-				expandedWords.add((hypernym,level))
+				nextStack.add(enhancedWord(hypernym,level,s.weight))
+				expandedWords.add(enhancedWord(hypernym,level,s.weight))
 
 		level += 1
 
-	return sorted(list(expandedWords),key=lambda x: x[1])
+	# return sorted(list(expandedWords),key=lambda x: x[1])
+	return sorted(list(expandedWords),key=lambda x: x.level)
 
 def sum_top(sims,n):
 	"""
@@ -103,7 +121,7 @@ def sum_top(sims,n):
 	return sum(topSims)
 
 def main():
-	srcdir = "/Users/jchan/Dropbox/Research/PostDoc/CrowdSchemas/WOZ/small_surface/"
+	srcdir = "/Users/jchan/Dropbox/Research/PostDoc/CrowdSchemas/WOZ/small_structure/"
 	documents = []
 	docNames = []
 	for f in os.listdir(srcdir):
@@ -124,8 +142,8 @@ def main():
 		docName1 = docNames[combo[0]]
 		docName2 = docNames[combo[1]]
 		print "Processing %s vs %s..." %(docName1,docName2)
-		sim = np.mean(find_paths(doc1,doc2))
-		# sim = sum_top(find_paths(doc1,doc2),10) # sum of top 10 path similarities
+		# sim = np.mean(find_paths(doc1,doc2))
+		sim = sum_top(find_paths(doc1,doc2),10) # sum of top 10 path similarities
 		results.append([docName1,docName2,sim,doc1,doc2])
 	# for i in xrange(len(documents)):
 	# 	print "Processing %s..." %docNames[i]
@@ -137,7 +155,7 @@ def main():
 	# 			sim = sum(find_paths(doc1,doc2))
 	# 			results.append([docNames[i],docNames[j],sim,doc1,doc2])
 
-	with open("/Users/jchan/Dropbox/Research/PostDoc/CrowdSchemas/WOZ/smallResults_surface_mean.csv",'w') as csvfile:
+	with open("/Users/jchan/Dropbox/Research/PostDoc/CrowdSchemas/WOZ/smallResults_structure_topSum.csv",'w') as csvfile:
 		csvwriter = csv.writer(csvfile)
 		csvwriter.writerow(['doc1','doc2','sim','words1','words2'])
 		for result in results:
